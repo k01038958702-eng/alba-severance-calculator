6976ad84297ef0d308ba91ae8edd4d5f41d32141
k01038958702-eng <k01038958702@gmail.com>
2026-07-25 17:29:10 +0900

    Add files via upload

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://severance.richchoi.kr/sitemap.xml",
+  };
+}
---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://severance.richchoi.kr/",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ];
+}
