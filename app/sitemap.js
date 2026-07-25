2570652c45d0c2f47a6f5d43bc532bb7ae8954e6
k01038958702-eng <k01038958702@gmail.com>
2026-07-25 18:13:25 +0900

    Add files via upload

--- app/sitemap.js
+++ app/sitemap.js
@@ -8,5 +8,11 @@ export default function sitemap() {
       changeFrequency: "monthly",
       priority: 1,
     },
+    {
+      url: "https://severance.richchoi.kr/privacy",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.3,
+    },
   ];
 }
